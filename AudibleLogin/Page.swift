4be3fd6681d50860cd4761780bda6debdc2ba4a7
Chase McElroy <ichasem@gmail.com>
2018-03-01 17:51:46 -0800

    Created Onboarding flow using collection vc

---
+++ AudibleLogin/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  AudibleLogin
+//
+//  Created by Chase McElroy on 3/1/18.
+//  Copyright © 2018 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+struct Page {
+    let title: String
+    let message: String
+    let image: UIImage
+}
